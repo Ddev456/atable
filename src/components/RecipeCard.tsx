e675a9e87a02f123f25a8d27923139da9f60c1ad
Ddev456 <dev.delhalle@gmail.com>
2023-03-11 11:30:14 +0100

    untracked files fixed

---
+++ src/components/RecipeCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { type Recipe } from '../pages/index'
+
+type RecipeCardProps = {
+  randomRecipe: Recipe | undefined
+}
+
+export const RecipeCard = ({randomRecipe}: RecipeCardProps) => {
+    console.log(randomRecipe);
+    
+  return (
+    <div className="card w-96 bg-base-100">
+              <figure><img src={randomRecipe?.strMealThumb} alt="thumbnail of recipe" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  {randomRecipe?.strMeal}
+                  <div className="badge badge-secondary">Nouveau</div>
+                </h2>
+                <p className='h-80 overflow-auto'>{randomRecipe?.strInstructions}</p>
+                <div className="card-actions justify-end">
+                <button
+                          className="inline-block px-4 py-3
+                          text-sm font-semibold text-center
+                          text-white uppercase transition
+                          duration-200 ease-in-out bg-[#67e8f9]
+                          rounded-md cursor-pointer
+                          hover:bg-[#0891b2] active:scale-90">
+                          Télécharger
+                        </button>
+                  </div>
+             </div>
+           </div>
+  )
+}
+
